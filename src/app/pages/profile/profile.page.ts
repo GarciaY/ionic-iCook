a23f77028c02fdd673ee391ae37fd0862eb65bb4
Yuri Garcia <201911021@gordoncollege.edu.ph>
2022-03-14 13:37:22 +0800

    modal update

--- src/app/pages/profile/profile.page.ts
+++ src/app/pages/profile/profile.page.ts
@@ -20,7 +20,7 @@ export class ProfilePage implements OnInit {
     },
     {
       label: 'Phone Number',
-      NumVal: 999999999,
+      TextVal: '09120909090',
       icon: 'call'
       //TextVal
     },
